13a9c4c22a2be95363f1bb079acde72f1bbe2fb6
Márton Kozma <kozmartinus@gmail.com>
2020-12-15 21:21:46 +0100

    modify routes in annotation

--- src/main/java/org/example/Routes.java
+++ src/main/java/org/example/Routes.java
@@ -2,14 +2,20 @@ package org.example;
 
 public class Routes {
 
-    @WebRoute("/test1")
-    public String test1() {
-        return "test1 value";
+    @WebRoute(method="GET", path = "/test1")
+    public String getTest1() {
+        return "test1 with GET method";
     }
 
-    @WebRoute("/test2")
-    public String test2() {
-        return "test2 value";
+    @WebRoute(method="POST", path = "/test1")
+    public String postTest1() {
+        return "test1 with POST method";
+    }
+
+
+    @WebRoute(method="GET", path = "/test2")
+    public String getTest2() {
+        return "test2 with GET method";
     }
 
 }
