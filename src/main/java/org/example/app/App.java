912645e546198df5c1bd7e421d2569f85e2ced38
Márton Kozma <kozmartinus@gmail.com>
2020-12-16 00:54:36 +0100

    add handler for extracting variables from the path

--- src/main/java/org/example/app/App.java
+++ src/main/java/org/example/app/App.java
@@ -3,10 +3,13 @@ package org.example.app;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -29,8 +32,20 @@ public class App {
                 String route = method.getAnnotation(WebRoute.class).path();
                 String methodType = method.getAnnotation(WebRoute.class).method();
                 Map routesOfType = (HashMap) routes.get(methodType);
-                routesOfType.put(route, method.invoke(Routes.class.getDeclaredConstructor().newInstance()));
-                server.createContext(route, new MyHandler());
+
+                String pattern = "<\\w+>";
+                Pattern r = Pattern.compile(pattern);
+                Matcher m = r.matcher(route);
+
+                if (m.find( )) {
+                    int signIndex = route.indexOf('<');
+                    String path = route.substring(0, signIndex - 1);
+                    server.createContext(path, new parameterHandler(signIndex, method));
+                } else {
+                    routesOfType.put(route, method.invoke(Routes.class.getDeclaredConstructor().newInstance()));
+                    server.createContext(route, new MyHandler());
+                }
+
             }
         }
         server.setExecutor(null);
@@ -44,6 +59,39 @@ public class App {
             String method = t.getRequestMethod();
             Map routesOfType = (HashMap) routes.get(method);
             String response = (String) routesOfType.get(path);
+
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class parameterHandler implements HttpHandler {
+        private final Method method;
+        private final int signIndex;
+
+        public parameterHandler(int number, Method method) {
+            this.signIndex = number;
+            this.method = method;
+        }
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String path = t.getRequestURI().toString();
+            String parameter = path.substring(this.signIndex);
+            String response = null;
+            try {
+                response = (String) this.method.invoke(Routes.class.getDeclaredConstructor().newInstance(), parameter);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
 
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
