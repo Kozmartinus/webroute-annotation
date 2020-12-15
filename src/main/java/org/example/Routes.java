95361119b19c1d971e3ae1f7d5d77539a6776bbd
Márton Kozma <kozmartinus@gmail.com>
2020-12-16 00:54:05 +0100

    add routes for extracting variables from the path

--- src/main/java/org/example/Routes.java
+++ src/main/java/org/example/Routes.java
@@ -12,10 +12,19 @@ public class Routes {
         return "test1 with POST method";
     }
 
-
     @WebRoute(method="GET", path = "/test2")
     public String getTest2() {
         return "test2 with GET method";
+    }
+
+    @WebRoute(method="GET", path = "/user/<userName>")
+    public String getHello(String name) {
+        return "hello " + name + "! with GET method";
+    }
+
+    @WebRoute(method="GET", path = "/picture/<pictureName>")
+    public String getPictureName(String name) {
+        return "picture's name: " + name + " with GET method";
     }
 
 }
